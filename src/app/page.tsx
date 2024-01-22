e0cfcb3cbde22fac3ef21c2df108f26d59baf7dd
croath <croathliu@gmail.com>
2024-01-22 14:31:41 +0900

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,220 @@
+'use client';
+import { useEffect, useState } from "react";
 import Image from "next/image";
+import classNames from 'classnames';
+import { Graph } from '@visx/network';
+import {
+  Provider,
+  BackendBehaviour,
+  debug,
+} from '@ringsnetwork/rings-node'
+// import init from "@ringsnetwork/rings-node/dist/rings_node"
+import { generatePrivateKey, privateKeyToAccount } from 'viem/accounts'
+
+function hexToBytes(hex: number | string) {
+  hex = hex.toString(16)
+  hex = hex.replace(/^0x/i, '')
+  for (var bytes : number[] = [], c = 0; c < hex.length; c += 2) bytes.push(parseInt(hex.slice(c, c + 2), 16))
+  return bytes
+}
+
+const service_message_handler = async (ctxRef: any, providerRef: any, msgCtx: any, msg: any) => { 
+  console.log('service_message_handler')
+  console.log(msg)
+}
+
+const plain_text_message_handler = async (ctxRef: any, providerRef: any, msgCtx: any, msg: any) => { 
+  console.log('plain_text_message_handler')
+  console.log(msg)
+}
+
+const extension_message_handler = async (ctxRef: any, providerRef: any, msgCtx: any, msg: any) => { 
+  console.log('extension_message_handler')
+  console.log(msg)
+}
+
 
 export default function Home() {
+  const [wasm, setWasm] = useState<any>(null)
+  const [nodes, setNodes] = useState<any>(null)
+  const [nodesData, setNodesData] = useState<any>(null)
+  useEffect(() => {
+    if (!wasm) {
+      const initWasm = async () => {
+        const w = await init()
+        debug(true)
+        setWasm(w)
+      }
+
+      initWasm()
+    }
+  }, [wasm])
+
+  useEffect(() => {
+    if (!wasm) {
+      return
+    }
+    console.log('wasm found')
+    
+    const generateNodesOnCircle = async (numberOfNodes: number, radius = 100, centerX = 250, centerY = 250) => {
+      let newNodes:any[] = [];
+      for (let i = 0; i < numberOfNodes; i++) {
+        const angle = 2 * Math.PI * i / numberOfNodes;
+        const x = centerX + radius * Math.cos(angle);
+        const y = centerY + radius * Math.sin(angle);
+
+        const privateKey = generatePrivateKey()
+        const account = privateKeyToAccount(privateKey)
+
+        const signer = async (proof: string): Promise<Uint8Array> => {
+            const signed = await account.signMessage({message: proof})
+            return new Uint8Array(hexToBytes(signed!));
+        }
+
+        const listen = async () => {
+          const context = new BackendBehaviour(
+            service_message_handler,
+            plain_text_message_handler,
+            extension_message_handler
+          )
+          let provider: Provider = await new Provider(
+            // ice_servers
+            'stun://stun.l.google.com:19302',
+            // stable_timeout
+            BigInt(60),
+            // account
+            account.address,
+            // account type
+            "eip191",
+            // signer
+            signer,
+            // callback
+            context
+          )
+          await provider.listen()
+          if (i > 0) {
+            const prevItem = newNodes[i-1];
+            const offer = await prevItem.provider.request("createOffer", [account.address])
+            const answer = await provider.request("answerOffer", [offer.result])
+            await prevItem.provider.request("acceptAnswer", [answer.result])
+            const info = await provider.request("nodeInfo", [])
+            console.log(info)
+          }
+          newNodes.push({ x, y, account, provider });
+        }
+        await listen()
+      }
+      setNodes(newNodes)
+    }
+
+    generateNodesOnCircle(8);
+  }, [wasm]);
+
+  const setupNodes = () => {
+
+  }
+
+  useEffect(() => {
+    if (nodes != null) {
+      console.log(nodes)
+      const dataSample = {
+        nodes,
+        links: [
+          { source: nodes[0], target: nodes[1] },
+          { source: nodes[1], target: nodes[2] },
+          { source: nodes[2], target: nodes[0] },
+        ],
+      };
+      setNodesData(dataSample)
+    }
+  }, [nodes]);
+
+  const MyGraph = () => {
+    if (nodes != null) {
+      console.log(nodesData)
+      return (<svg width="500" height="500"> <Graph graph={nodesData} /></svg>)
+    }
+    return (<svg width="500" height="500"></svg>)
+  };
+
+  const InfoTable = () => {
+    var nodeElements = [];
+    for (let i = 0; i < nodes.length; i++) {
+      const node = nodes[i];
+      nodeElements.push(<a
+        className={nodeClass}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <h2 className={`mb-3 text-2xl font-semibold`}>
+          Node{" " + i}
+          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            -&gt;
+          </span>
+        </h2>
+        <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+          {node.account.address.substring(0,6)}
+        </p>
+      </a>)
+    }
+    return (
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+      {nodeElements}
+    </div>
+    )
+  }
+
+  const buttonClass = classNames(
+    "fixed",
+    "left-0",
+    "top-0",
+    "flex",
+    "w-full",
+    "justify-center",
+    "border-b",
+    "border-gray-300",
+    "bg-gradient-to-b from-zinc-200 dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-2 lg:dark:bg-zinc-800/30"
+  )
+
+  const nodeClass = classNames(
+    "group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+  )
+  
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+          Decentralized Proof Demo&nbsp;
+          <a href="https://github.com/RingsNetwork/rings-proof-demo" target="_blank"><code className="font-mono font-bold">[Source Code]</code></a>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="https://ringsnetwork.io"
             target="_blank"
             rel="noopener noreferrer"
           >
             By{" "}
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
+              src="/ringslogo.svg"
+              alt="Rings Network Logo"
               className="dark:invert"
-              width={100}
+              width={240}
               height={24}
               priority
             />
           </a>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <p className={buttonClass} onClick={setupNodes}>1. Start 8 rings node</p>
+        <p className={buttonClass} >2. Connect them to a local network</p>
+        <p className={buttonClass} >3. Run a proof job on 1 node</p>
+        <p className={buttonClass} >3. Run Rings Proof</p>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <InfoTable />
+      <MyGraph />
     </main>
   );
 }
